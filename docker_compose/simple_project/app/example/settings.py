3cb737b9ef048101e07aa196a834a48178439aed
Дмитрий Пимкин <pimkin@gmail.com>
2025-10-03 14:02:23 +0400

    прогнал тесты - corsheaders

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -38,14 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'movies',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -162,4 +160,3 @@ STATIC_ROOT = '/var/www/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080",]
